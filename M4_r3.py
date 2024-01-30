ce04b4f8741464f2bc020800d20878e5bb868f05
IgorIlnicki <symbol_t@ukr.net>
2024-01-30 10:55:13 +0200

    a1

---
+++ M4_r3.py
@@ -0,0 +1,4 @@
+# Перетворення списку чисел у байт-рядок
+numbers = [0, 128, 255]
+byte_numbers = bytes(numbers)
+print(byte_numbers)  # Виведе байтове представлення чисел
---
+++ M4_r4.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+# Перетворення відносного шляху в абсолютний
+# relative_path = Path("Zadachki\M4_r4.py")
+# absolute_path = relative_path.absolute()
+# print(absolute_path)
+
+# current_working_directory = Path("D:\Project\Dom_Zad\Zadachki\M4_r4.py")
+# relative_path = absolute_path.relative_to(current_working_directory)
+# print(relative_path)
+
+# original_path = Path("documents/example.txt")
+
+# # Створює новий об'єкт Path з іншим ім'ям файлу
+# new_path = original_path.with_name("report.txt")
+# original_path.rename(new_path)
+
+# path = Path("./picture")
+
+# # Перевірка існування
+# if path.exists():
+#     print(f"{path} існує")
+
+# # Перевірка, чи це директорія
+# if path.is_dir():
+#     print(f"{path} є директорією")
+
+# # Перевірка, чи це файл
+# if path.is_file():
+#     print(f"{path} є файлом")
