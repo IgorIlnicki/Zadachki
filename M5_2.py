d7ccd3062a22c783a93ba2131740b309122c8c97
IgorIlnicki <symbol_t@ukr.net>
2024-02-04 13:58:49 +0200

    A5

--- M5_2.py
+++ M5_2.py
@@ -1,23 +1,22 @@
-words = ['apple', 'zoo', 'lion', 'bet', 'wolf', 'lama', 'bear', 'appendix']
-grouped_words = {}
-
-for word in words:
-    char = word[0]
-    if char not in grouped_words:
-        print(char)
-        grouped_words[char] = []
-        
-    grouped_words[char].append(word)
-    print(f"grouped_words[{char}] = {grouped_words[char]}")
-print(grouped_words)
-
-# from collections import defaultdict
-
-# words = ['apple', 'zoo', 'lion', 'lama', 'bear', 'bet', 'wolf', 'appendix']
-# grouped_words = defaultdict(list)
+# words = ['apple', 'zoo', 'lion', 'bet', 'wolf', 'lama', 'bear', 'appendix']
+# grouped_words = {}
 
 # for word in words:
 #     char = word[0]
+#     if char not in grouped_words:
+#         print(char)
+#         grouped_words[char] = []
+        
 #     grouped_words[char].append(word)
+#     print(f"grouped_words[{char}] = {grouped_words[char]}")
+# print(grouped_words)
 
-# print(dict(grouped_words))
+from collections import defaultdict
+
+words = ['apple', 'zoo', 'lion', 'lama', 'bear', 'bet', 'wolf', 'appendix']
+grouped_words = defaultdict(list)
+for word in words:
+    char = word[0]
+    grouped_words[char].append(word)
+   
+print(dict(grouped_words))
